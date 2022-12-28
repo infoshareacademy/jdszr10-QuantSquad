2a33eb94de28aaf302f4a4880e26049f40a3d575
Piotr Rzepka <rzepki@gmail.com>
2022-12-28 20:45:28 +0100

    import dbdump script added for prod and test db

---
+++ projekt_sql/02_Oczyszczone_dane/04_import_dbdump.bat
@@ -0,0 +1,6 @@
+@REM Create 2 databases happiness_prod and happiness_test and import previously saved data (same file into both databases)
+@REM Made as separate file (instead of query within the database server) to make it easier to import by other team members 
+createdb -U postgres happiness_prod
+createdb -U postgres happiness_test
+psql -U postgres -d happiness_prod -f happiness_allyears_only.sql
+psql -U postgres -d happiness_test -f happiness_allyears_only.sql
